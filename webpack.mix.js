7d85cb81930bf19cedf9ac83ba0ca92ae1670dd5
drissboumlik <idrissboumlik@gmail.com>
2022-02-17 23:43:30 +0100

    Edit webpack config

---
+++ webpack.mix.js
@@ -0,0 +1,34 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+mix.pug = require('laravel-mix-pug');
+
+mix
+    .copy('src/dist/fonts', 'dist/fonts')
+    .copy('src/dist/assets', 'dist/assets')
+    .js('./src/js/app.js', 'dist/js')
+    .sass('./src/sass/externals.sass', 'css')
+    .sass('./src/sass/app.sass', 'css')
+    .pug('src/*.pug', '../dist')
+    .setPublicPath('./dist')
+    // .purgeCss({
+    //     extend: {
+    //         content: [path.join(__dirname, 'database/data/**/*.json')],
+    //     },
+    // })
+    // .postCss()
+
+    .sourceMaps(true, 'source-map')
+
+    /* Tools */
+    .browserSync({server: 'dist', proxy: null})
+    // .disableNotifications()
+    /* Options */
+    // .options({
+    //     processCssUrls: false
+    // })
+    ;
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
